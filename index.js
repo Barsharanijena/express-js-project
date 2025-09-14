59b59f86940c67ebff545788f46b9e6082fc08ce
Barsharani Jena <Barsharanijena555@gmail.com>
2025-09-14 13:14:50 +0530

    server created

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 const express = require('express');
+const app = express();
+
+const Port = 3000;
+
+app.get('/',(req,res)=>{
+    res.send("hello")
+
+})
+
+
+app.listen(Port,()=>{
+ console.log(`server is  now running at ${Port}`)
+})
